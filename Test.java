9aa13a871a6041e3406ab73b97a9512659a0d1ef
Jaidersheni <dershenijai@gmail.com>
2016-08-13 12:21:31 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class Test {
 public static void main(String []arg){
 	int Input;
+	String Output;
 	Scanner sc=new Scanner(System.in);
 	Input=sc.nextInt();
-	if(Input>0)
+	if(Input>0){
 		System.out.println("Positive");
-	else if(Input<0)
+	Output="Positive";}
+	else if(Input<0){
 	System.out.println("Negative");
-	else
+	Output="Negative";}
+	else{
 		System.out.println("Zero");
+		Output="Zero";}
 }
 }

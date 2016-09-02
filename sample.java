b579b0acebb7ec526f44fa7053c1240def6c5de0
Jaidersheni <dershenijai@gmail.com>
2016-09-02 05:49:38 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.util.*;	
+
+public class sample {
+
+	    public static void main(String a[]){
+	    	Scanner sc=new Scanner(System.in);
+	    	System.out.println("Enter the string");
+	    	String Input=sc.next();
+	    	int Output=Integer.valueOf(Input);
+	    	System.out.println(Output);
+	    }
+	}
+	
+
